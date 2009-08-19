158116698a134291ffc78ebda847667f76304ff7
Paul Battley <pbattley@gmail.com>
2009-08-19 13:45:47 +0100

    ivars are quicker than method calls.

--- lib/htmlentities/decoder.rb
+++ lib/htmlentities/decoder.rb
@@ -2,24 +2,21 @@ class HTMLEntities
   class Decoder #:nodoc:
     def initialize(flavor)
       @flavor = flavor
+      @map = HTMLEntities::MAPPINGS[@flavor]
     end
 
     def decode(source)
       source.to_s.gsub(named_entity_regexp) {
-        (cp = map[$1]) ? [cp].pack('U') : $&
+        (cp = @map[$1]) ? [cp].pack('U') : $&
       }.gsub(/&#([0-9]{1,7});|&#x([0-9a-f]{1,6});/i) {
         $1 ? [$1.to_i].pack('U') : [$2.to_i(16)].pack('U')
       }
     end
 
   private
-    def map
-      @map ||= HTMLEntities::MAPPINGS[@flavor]
-    end
-
     def named_entity_regexp
       @named_entity_regexp ||= (
-        key_lengths = map.keys.map{ |k| k.length }
+        key_lengths = @map.keys.map{ |k| k.length }
         ok_chars = @flavor.to_s == 'expanded' ? '(?:b\.)?[a-z][a-z0-9]' : '[a-z][a-z0-9]'
         /&(#{ ok_chars }{#{ key_lengths.min - 1 },#{ key_lengths.max - 1 }});/i
       )
