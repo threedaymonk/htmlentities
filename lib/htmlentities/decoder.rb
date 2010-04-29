b2f584fb2f3f2875c5ebdd3aaf386cb91df2950e
Paul Battley <pbattley@gmail.com>
2010-04-29 10:42:57 +0100

    Improved code readability.

--- lib/htmlentities/decoder.rb
+++ lib/htmlentities/decoder.rb
@@ -8,7 +8,7 @@ class HTMLEntities
 
     def decode(source)
       source.to_s.gsub(@named_entity_regexp) {
-        (cp = @map[$1]) ? [cp].pack('U') : $&
+        (codepoint = @map[$1]) ? [codepoint].pack('U') : $&
       }.gsub(/&#(?:([0-9]{1,7})|x([0-9a-f]{1,6}));/i) {
         $1 ? [$1.to_i].pack('U') : [$2.to_i(16)].pack('U')
       }
@@ -17,8 +17,13 @@ class HTMLEntities
   private
     def named_entity_regexp
       key_lengths = @map.keys.map{ |k| k.length }
-      ok_chars = @flavor == 'expanded' ? '(?:b\.)?[a-z][a-z0-9]' : '[a-z][a-z0-9]'
-      /&(#{ ok_chars }{#{ key_lengths.min - 1 },#{ key_lengths.max - 1 }});/i
+      entity_name_pattern =
+        if @flavor == 'expanded'
+          '(?:b\.)?[a-z][a-z0-9]'
+        else
+          '[a-z][a-z0-9]'
+        end
+      /&(#{ entity_name_pattern }{#{ key_lengths.min - 1 },#{ key_lengths.max - 1 }});/i
     end
   end
 end
