7b1f470e22c44e5097d512d2d3ea57db70c02721
Paul Battley <pbattley@gmail.com>
2011-11-30 12:34:15 +0000

    Only run the interoperability tests if an environment variable is set.

--- test/interoperability_test.rb
+++ test/interoperability_test.rb
@@ -1,13 +1,15 @@
 # encoding: UTF-8
 require File.expand_path("../common", __FILE__)
 
-class HTMLEntities::InteroperabilityTest < Test::Unit::TestCase
+if ENV["RUN_INTEROPERABILITY_TESTS"]
+  class HTMLEntities::InteroperabilityTest < Test::Unit::TestCase
 
-  def test_should_encode_active_support_safe_buffer
-    require 'active_support'
-    string = "<p>This is a test</p>"
-    buffer = ActiveSupport::SafeBuffer.new(string)
-    coder = HTMLEntities.new
-    assert_equal coder.encode(string, :named), coder.encode(buffer, :named)
+    def test_should_encode_active_support_safe_buffer
+      require 'active_support'
+      string = "<p>This is a test</p>"
+      buffer = ActiveSupport::SafeBuffer.new(string)
+      coder = HTMLEntities.new
+      assert_equal coder.encode(string, :named), coder.encode(buffer, :named)
+    end
   end
 end
